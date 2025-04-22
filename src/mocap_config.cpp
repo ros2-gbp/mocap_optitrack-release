2684e8e988781064963344e79b8f9ab8f94779a5
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2025-04-22 00:08:31 -0400

    Rebase from 'debian/humble/mocap_optitrack'

--- src/mocap_config.cpp
+++ src/mocap_config.cpp
@@ -1,30 +1,30 @@
-/* 
+/*
  * Copyright (c) 2018, Houston Mechatronics Inc., JD Yamokoski
  * Copyright (c) 2012, Clearpath Robotics, Inc., Alex Bencz
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright notice, 
+ * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in the 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its 
- *    contributors may be used to endorse or promote products derived from 
- *    this software without specific prior written permission. 
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <string>
@@ -46,27 +46,27 @@ ServerDescription::ServerDescription() :
 {}
 
 void NodeConfiguration::fromRosParam(
-  rclcpp::Node::SharedPtr& node, 
-  ServerDescription& serverDescription, 
+  rclcpp::Node::SharedPtr& node,
+  ServerDescription& serverDescription,
   PublisherConfigurations& pubConfigs)
 {
-  // Get server cconfiguration from ROS parameter server
-  
+  // Get server configuration from ROS parameter server
+
   if(!node->get_parameter_or(
-    rosparam::keys::MulticastIpAddress, 
-    serverDescription.multicastIpAddress, 
+    rosparam::keys::MulticastIpAddress,
+    serverDescription.multicastIpAddress,
     ServerDescription::Default::MulticastIpAddress)
   ) {
-    RCLCPP_WARN(node->get_logger(), 
+    RCLCPP_WARN(node->get_logger(),
       "Could not get multicast address, using default: %s", serverDescription.multicastIpAddress.c_str());
   }
 
   if(!node->get_parameter_or(
-    rosparam::keys::CommandPort, 
-    serverDescription.commandPort, 
+    rosparam::keys::CommandPort,
+    serverDescription.commandPort,
     ServerDescription::Default::CommandPort)
   ) {
-    RCLCPP_WARN(node->get_logger(), 
+    RCLCPP_WARN(node->get_logger(),
       "Could not get command port, using default: %i", serverDescription.commandPort);
   }
 
@@ -80,22 +80,22 @@ void NodeConfiguration::fromRosParam(
   }
 
   if(!node->get_parameter_or(
-    rosparam::keys::DataPort, 
-    serverDescription.dataPort, 
+    rosparam::keys::DataPort,
+    serverDescription.dataPort,
     ServerDescription::Default::DataPort)
   ) {
-    RCLCPP_WARN(node->get_logger(), 
+    RCLCPP_WARN(node->get_logger(),
       "Could not get data port, using default: %i", serverDescription.dataPort);
   }
-  
+
   if(!node->get_parameter(
-    rosparam::keys::Version, 
+    rosparam::keys::Version,
     serverDescription.version)
   ) {
-    RCLCPP_WARN(node->get_logger(), 
+    RCLCPP_WARN(node->get_logger(),
       "Could not get server version, using auto");
   }
-  
+
   const std::vector<std::string> prefix{rosparam::keys::RigidBodies};
   const std::string name_with_dot = std::string(node->get_name()) + ".";
   const auto result = node->get_node_parameters_interface()->list_parameters(
@@ -119,9 +119,9 @@ void NodeConfiguration::fromRosParam(
                                                                         "pose", param_desc);
     if (publisherConfig.poseTopicName.empty())
     {
-      RCLCPP_WARN(node->get_logger(), 
-        "Failed to parse %s for body %d. Pose publishing disabled.", 
-        rosparam::keys::PoseTopicName.c_str(), 
+      RCLCPP_WARN(node->get_logger(),
+        "Failed to parse %s for body %d. Pose publishing disabled.",
+        rosparam::keys::PoseTopicName.c_str(),
         publisherConfig.rigidBodyId);
 
       publisherConfig.publishPose = false;
@@ -150,7 +150,7 @@ void NodeConfiguration::fromRosParam(
     {
       publisherConfig.publishPose2d = true;
     }
-  
+
     if (node->has_parameter(prefix + "." + rosparam::keys::OdomTopicName))
     {
       node->undeclare_parameter(prefix + "." + rosparam::keys::OdomTopicName);
@@ -192,7 +192,7 @@ void NodeConfiguration::fromRosParam(
     }
     publisherConfig.childFrameId = node->declare_parameter(prefix + "." + rosparam::keys::ChildFrameId,
                                                            "base_link", param_desc);
-      
+
     if (node->has_parameter(prefix + "." + rosparam::keys::ParentFrameId))
     {
       node->undeclare_parameter(prefix + "." + rosparam::keys::ParentFrameId);
